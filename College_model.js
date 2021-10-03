8c7b1f68e20a86ed2abaf8a0ed7520494dcfd7f2
gnambiar63 <52907595+gnambiar63@users.noreply.github.com>
2021-10-03 20:49:45 +0530

    Version 1.0.0

---
+++ College_model.js
@@ -0,0 +1,148 @@
+//College JSON data
+// export 
+const College_Data = [
+    {
+        "College_Name" : "KCG",
+        "Annual_Fee" : "10000",
+        "Caste" : "GEN",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "50000",
+        "Scholarship_Marks" : "85"
+    },
+    {
+        "College_Name" : "KCG",
+        "Annual_Fee" : "8000",
+        "Caste" : "SC",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "60000",
+        "Scholarship_Marks" : "75"
+    },
+    {
+        "College_Name" : "KCG",
+        "Annual_Fee" : "7000",
+        "Caste" : "ST",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "60000",
+        "Scholarship_Marks" : "70"
+    },
+    {
+        "College_Name" : "KCG",
+        "Annual_Fee" : "20000",
+        "Caste" : "GEN",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "60000",
+        "Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "KCG",
+        "Annual_Fee" : "15000",
+        "Caste" : "SC",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "40000",
+        "Scholarship_Marks" : "75"
+    },
+    {
+        "College_Name" : "KCG",
+        "Annual_Fee" : "10000",
+        "Caste" : "ST",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "40000",
+        "Scholarship_Marks" : "70"
+    },
+    {
+        "College_Name" : "GCELT",
+        "Annual_Fee" : "30000",
+        "Caste" : "GEN",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "30000",
+        "Scholarship_Marks" : "80"
+    },
+    {
+        "College_Name" : "GCELT",
+        "Annual_Fee" : "25000",
+        "Caste" : "SC",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "40000",
+        "Scholarship_Marks" : "60"
+    },
+    {
+        "College_Name" : "GCELT",
+        "Annual_Fee" : "20000",
+        "Caste" : "ST",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "30000",
+        "Scholarship_Marks" : "50"
+    },
+    {
+        "College_Name" : "GCELT",
+        "Annual_Fee" : "80000",
+        "Caste" : "GEN",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "90000",
+        "Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "GCELT",
+        "Annual_Fee" : "50000",
+        "Caste" : "SC",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "95000",
+        "Scholarship_Marks" : "70"
+    },
+    {
+        "College_Name" : "GCELT",
+        "Annual_Fee" : "40000",
+        "Caste" : "ST",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "95000",
+        "Scholarship_Marks" : "60"
+    },
+    {
+        "College_Name" : "SPIT",
+        "Annual_Fee" : "140000",
+        "Caste" : "GEN",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "70000",
+        "Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "SPIT",
+        "Annual_Fee" : "120000",
+        "Caste" : "SC",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "90000",
+        "Scholarship_Marks" : "80"
+    },
+    {
+        "College_Name" : "SPIT",
+        "Annual_Fee" : "120000",
+        "Caste" : "ST",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "80000",
+        "Scholarship_Marks" : "70"
+    },
+    {
+        "College_Name" : "SPIT",
+        "Annual_Fee" : "160000",
+        "Caste" : "GEN",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "90000",
+        "Scholarship_Marks" : "95"
+    },
+    {
+        "College_Name" : "SPIT",
+        "Annual_Fee" : "120000",
+        "Caste" : "SC",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "95000",
+        "Scholarship_Marks" : "80"
+    },
+    {
+        "College_Name" : "SPIT",
+        "Annual_Fee" : "120000",
+        "Caste" : "ST",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "100000",
+        "Scholarship_Marks" : "80"
+    },
+];
